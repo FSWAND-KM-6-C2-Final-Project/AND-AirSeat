65e1bae8ba605e46eb91a08ee750538e1f76a37e
rafiammr <rafiammr>
2024-05-26 00:04:30 +0700

    Add Layouting Webview Midtrans

--- app/src/main/java/com/nafi/airseat/presentation/flightdetail/FlightDetailActivity.kt
+++ app/src/main/java/com/nafi/airseat/presentation/flightdetail/FlightDetailActivity.kt
@@ -1,21 +1,28 @@
 package com.nafi.airseat.presentation.flightdetail
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.nafi.airseat.R
+import com.nafi.airseat.databinding.ActivityFlightDetailBinding
+import com.nafi.airseat.presentation.payment.WebViewMidtransActivity
 
 class FlightDetailActivity : AppCompatActivity() {
+    private val binding: ActivityFlightDetailBinding by lazy {
+        ActivityFlightDetailBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_flight_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.cvSectionCheckout.btnPayment.setOnClickListener {
+            openUrlFromWebView()
         }
+    }
+
+    private fun openUrlFromWebView() {
+        val intent = Intent(this, WebViewMidtransActivity::class.java)
+        intent.putExtra("URL", "https://sample-demo-dot-midtrans-support-tools.et.r.appspot.com/snap-redirect/")
+        startActivity(intent)
     }
 }
