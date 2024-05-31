9c900c5c8c7168d85192d26ec1c3afa545505cfc
Novejira Angela <156919887+Novejira@users.noreply.github.com>
2024-05-31 14:28:50 +0700

    add reset password update

--- app/src/main/java/com/nafi/airseat/presentation/resetpasswordverifyemail/ReqChangePasswordViewModel.kt
+++ app/src/main/java/com/nafi/airseat/presentation/resetpasswordverifyemail/ReqChangePasswordViewModel.kt
@@ -6,21 +6,6 @@ import com.nafi.airseat.data.repository.UserRepository
 import kotlinx.coroutines.Dispatchers
 
 class ReqChangePasswordViewModel(private val repository: UserRepository) : ViewModel() {
-
-    fun verifChangePasswordOtp(
-        code: String,
-        email: String,
-        password: String,
-        confirmPassword: String,
-    ) = repository
-        .verifChangePasswordOtp(
-            email = email,
-            code = code,
-            password = password,
-            confirmPassword = confirmPassword,
-        )
-        .asLiveData(Dispatchers.IO)
-
     fun reqChangePasswordByEmail(email: String) =
         repository
             .reqChangePasswordByEmail(email)
