2a08793be219346ff03eee905ecf27d57b8c08a9
rafiammr <rafiammr>
2024-06-11 00:06:42 +0700

    Add groupie passenger and data model seat

--- app/src/main/java/com/nafi/airseat/di/AppModules.kt
+++ app/src/main/java/com/nafi/airseat/di/AppModules.kt
@@ -3,6 +3,13 @@ package com.nafi.airseat.di
 import android.content.SharedPreferences
 import com.nafi.airseat.core.BaseViewModel
 import com.nafi.airseat.data.datasource.AuthDataSource
+import com.nafi.airseat.data.datasource.AuthService
+import com.nafi.airseat.data.datasource.AuthServiceImpl
+import com.nafi.airseat.data.datasource.seat.SeatApiDataSource
+import com.nafi.airseat.data.datasource.seat.SeatDataSource
+import com.nafi.airseat.data.network.services.AirSeatApiService
+import com.nafi.airseat.data.repository.SeatRepository
+import com.nafi.airseat.data.repository.SeatRepositoryImpl
 import com.nafi.airseat.data.datasource.AuthDataSourceImpl
 import com.nafi.airseat.data.datasource.HistoryDataSource
 import com.nafi.airseat.data.datasource.HistoryDataSourceImpl
@@ -60,6 +67,7 @@ import com.nafi.airseat.presentation.profile.ProfileViewModel
 import com.nafi.airseat.presentation.register.RegisterViewModel
 import com.nafi.airseat.presentation.resetpassword.ResetPasswordViewModel
 import com.nafi.airseat.presentation.resetpasswordverifyemail.ReqChangePasswordViewModel
+import com.nafi.airseat.presentation.seatbook.SeatViewModel
 import com.nafi.airseat.presentation.resultsearch.ResultSearchViewModel
 import com.nafi.airseat.presentation.searchticket.SearchTicketViewModel
 import com.nafi.airseat.utils.SharedPreferenceUtils
@@ -95,6 +103,8 @@ object AppModules {
 
     private val datasource =
         module {
+            single<AuthDataSource> { APIAuthDataSource(get()) }
+            single<SeatDataSource> { SeatApiDataSource(get()) }
             single<AuthDataSource> { AuthDataSourceImpl(get()) }
             single<UserPrefDataSource> { UserPrefDataSourceImpl(get()) }
             single<NotificationDataSource> { NotificationDataSourceImpl(get()) }
@@ -112,6 +122,7 @@ object AppModules {
     private val repository =
         module {
             single<UserRepository> { UserRepositoryImpl(get()) }
+            single<SeatRepository> { SeatRepositoryImpl(get()) }
             single<AirportRepository> { AirportRepositoryImpl(get()) }
             single<FavoriteDestinationRepository> { FavoriteDestinationRepositoryImpl(get()) }
             single<SeatClassRepository> { SeatClassRepositoryImpl(get()) }
@@ -130,6 +141,8 @@ object AppModules {
             viewModelOf(::PassengerBioViewModel)
             viewModelOf(::HomeViewModel)
             viewModelOf(::LoginViewModel)
+            viewModelOf(::SeatViewModel)
+
             viewModel {
                 RegisterViewModel(get())
             }
--- app/src/main/java/com/nafi/airseat/presentation/biodata/OrdererBioViewModel.kt
+++ app/src/main/java/com/nafi/airseat/presentation/biodata/OrdererBioViewModel.kt
@@ -12,4 +12,49 @@ class OrdererBioViewModel : ViewModel() {
         val currentValue = _isFamilyNameMode.value ?: false
         _isFamilyNameMode.value = !currentValue
     }
+
+    private val _adultCount = MutableLiveData(0)
+    val adultCount: LiveData<Int> get() = _adultCount
+
+    private val _childCount = MutableLiveData(0)
+    val childCount: LiveData<Int> get() = _childCount
+
+    private val _babyCount = MutableLiveData(0)
+    val babyCount: LiveData<Int> get() = _babyCount
+
+    fun addAdult() {
+        val count = _adultCount.value ?: 0
+        _adultCount.value = count + 1
+    }
+
+    fun subAdult() {
+        val count = _adultCount.value ?: 0
+        if (count > 1) {
+            _adultCount.value = count - 1
+        }
+    }
+
+    fun addChild() {
+        val count = _childCount.value ?: 0
+        _childCount.value = count + 1
+    }
+
+    fun subChild() {
+        val count = _childCount.value ?: 0
+        if (count > 0) {
+            _childCount.value = count - 1
+        }
+    }
+
+    fun addBaby() {
+        val count = _babyCount.value ?: 0
+        _babyCount.value = count + 1
+    }
+
+    fun subBaby() {
+        val count = _babyCount.value ?: 0
+        if (count > 0) {
+            _babyCount.value = count - 1
+        }
+    }
 }
