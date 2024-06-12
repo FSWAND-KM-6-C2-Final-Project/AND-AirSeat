056f48822e33416ba2469c614bf3c19ac807c476
asnafialkaromi <asnafialkaromi4@gmail.com>
2024-06-12 18:42:16 +0700

    Update history model

--- app/src/main/java/com/nafi/airseat/data/mapper/HistoryMapper.kt
+++ app/src/main/java/com/nafi/airseat/data/mapper/HistoryMapper.kt
@@ -4,10 +4,16 @@ import com.nafi.airseat.data.model.History
 import com.nafi.airseat.data.model.HistoryArrivalAirport
 import com.nafi.airseat.data.model.HistoryDepartureAirport
 import com.nafi.airseat.data.model.HistoryFlight
+import com.nafi.airseat.data.model.HistoryReturnArrivalAirport
+import com.nafi.airseat.data.model.HistoryReturnDepartureAirport
+import com.nafi.airseat.data.model.HistoryReturnFlight
 import com.nafi.airseat.data.source.network.model.history.ArrivalAirport
 import com.nafi.airseat.data.source.network.model.history.Booking
 import com.nafi.airseat.data.source.network.model.history.DepartureAirport
 import com.nafi.airseat.data.source.network.model.history.Flight
+import com.nafi.airseat.data.source.network.model.history.ReturnArrivalAirport
+import com.nafi.airseat.data.source.network.model.history.ReturnDepartureAirport
+import com.nafi.airseat.data.source.network.model.history.ReturnFlight
 
 fun Booking?.toHistoryModel() =
     History(
@@ -16,16 +22,27 @@ fun Booking?.toHistoryModel() =
         classes = this?.classes.orEmpty(),
         createdAt = this?.createdAt.orEmpty(),
         duration = this?.duration.orEmpty(),
-        flight = this?.flight?.toHistoryFlightModel(),
-        returnFlight = this?.returnFlight?.toHistoryFlightModel(),
+        flight = this?.flight.toHistoryFlightModel(),
+        returnFlight = this?.returnFlight?.toHistoryReturnFlightModel(),
         totalAmount = this?.totalAmount.orEmpty()
     )
 
 fun Flight?.toHistoryFlightModel() =
     HistoryFlight(
-        arrivalAirport = this?.arrivalAirport?.toHistoryArrivalAirportModel(),
+        arrivalAirport = this?.arrivalAirport.toHistoryArrivalAirportModel(),
         arrivalTime = this?.arrivalTime.orEmpty(),
-        departureAirport = this?.departureAirport?.toHistoryDepartureAirportModel(),
+        departureAirport = this?.departureAirport.toHistoryDepartureAirportModel(),
+        departureTerminal = this?.departureTerminal.orEmpty(),
+        departureTime = this?.departureTime.orEmpty(),
+        flightNumber = this?.flightNumber.orEmpty(),
+        information = this?.information.orEmpty()
+    )
+
+fun ReturnFlight?.toHistoryReturnFlightModel() =
+    HistoryReturnFlight(
+        arrivalAirport = this?.arrivalAirport.toHistoryReturnArrivalAirportModel(),
+        arrivalTime = this?.arrivalTime.orEmpty(),
+        departureAirport = this?.departureAirport.toHistoryReturnDepartureAirportModel(),
         departureTerminal = this?.departureTerminal.orEmpty(),
         departureTime = this?.departureTime.orEmpty(),
         flightNumber = this?.flightNumber.orEmpty(),
@@ -41,8 +58,26 @@ fun DepartureAirport?.toHistoryDepartureAirportModel() =
         airportPicture = this?.airportPicture.orEmpty()
     )
 
+fun ReturnDepartureAirport?.toHistoryReturnDepartureAirportModel() =
+    HistoryReturnDepartureAirport(
+        airportCity = this?.airportCity.orEmpty(),
+        airportCityCode = this?.airportCityCode.orEmpty(),
+        airportContinent = this?.airportContinent.orEmpty(),
+        airportName = this?.airportName.orEmpty(),
+        airportPicture = this?.airportPicture.orEmpty()
+    )
+
 fun ArrivalAirport?.toHistoryArrivalAirportModel() =
     HistoryArrivalAirport(
+        airportCity = this?.airportCity.orEmpty(),
+        airportCityCode = this?.airportCityCode.orEmpty(),
+        airportContinent = this?.airportContinent.orEmpty(),
+        airportName = this?.airportName.orEmpty(),
+        airportPicture = this?.airportPicture.orEmpty()
+    )
+
+fun ReturnArrivalAirport?.toHistoryReturnArrivalAirportModel() =
+    HistoryReturnArrivalAirport(
         airportCity = this?.airportCity.orEmpty(),
         airportCityCode = this?.airportCityCode.orEmpty(),
         airportContinent = this?.airportContinent.orEmpty(),
