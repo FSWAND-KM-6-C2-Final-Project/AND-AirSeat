31465aa9ce9fefd6e6b23c20c45922c31bd72ddc
rafiammr <rafiammr>
2024-06-18 21:48:43 +0700

    Update flight detail price

---
+++ app/src/main/java/com/nafi/airseat/data/source/network/model/booking/PaymentData.kt
@@ -0,0 +1,10 @@
+package com.nafi.airseat.data.source.network.model.booking
+
+import com.google.gson.annotations.SerializedName
+
+data class PaymentData(
+    @SerializedName("token")
+    val token: String,
+    @SerializedName("redirect_url")
+    val redirectUrl: String
+)
