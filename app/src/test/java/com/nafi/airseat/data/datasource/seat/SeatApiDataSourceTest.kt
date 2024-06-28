a8afa4480cc6f65aa3928a7ff8e17e8a37789c52
rafiammr <rafiammr>
2024-06-28 09:12:32 +0700

    Add testing datasource seat and booking

---
+++ app/src/test/java/com/nafi/airseat/data/datasource/seat/SeatApiDataSourceTest.kt
@@ -0,0 +1,37 @@
+package com.nafi.airseat.data.datasource.seat
+
+import com.nafi.airseat.data.source.network.model.seat.SeatResponse
+import com.nafi.airseat.data.source.network.services.AirSeatApiServiceWithAuthorization
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class SeatApiDataSourceTest {
+    @MockK
+    lateinit var service: AirSeatApiServiceWithAuthorization
+
+    private lateinit var dataSource: SeatDataSource
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        dataSource = SeatApiDataSource(service)
+    }
+
+    @Test
+    fun getSeats() {
+        runTest {
+            val mockResponse = mockk<SeatResponse>(relaxed = true)
+            coEvery { service.getSeatData(any(), any()) } returns mockResponse
+            val actualResult = dataSource.getSeats("1", "Economy")
+            coVerify { service.getSeatData(any(), any()) }
+            assertEquals(mockResponse, actualResult)
+        }
+    }
+}
